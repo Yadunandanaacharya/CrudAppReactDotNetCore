dd4e9b42cd54e8de6ed987d845bb3b66a9fcf94d
Yadunandana Acharya <51967645+Yadunandanaacharya@users.noreply.github.com>
2025-04-24 08:13:29 +0530

    changes

---
+++ CRUD/Models/DCandidate.cs.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string fullName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public int mobile { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public int email { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public int age { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public int bloodGroup { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public int address { get; set; }
+    }
+}
